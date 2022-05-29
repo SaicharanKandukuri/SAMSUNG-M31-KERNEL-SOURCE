08db86ab82e5031f644ee66f88b6435bc52531b4
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-05-29 19:19:19 +0530

    patch setup script

--- setup.sh
+++ setup.sh
@@ -8,6 +8,5 @@ apt-get install -y sudo 'libtinfo*' build-essential wget
 
 # Download & extract toolchain
 wget https://github.com/SaicharanKandukuri/SAMSUNG-M31-KERNEL-SOURCE/releases/download/M315FXXU2CVCE-alpha/toolchain.tar.gz
-mkdir toolchain
-tar -xvf toolchain.tar.gz -C toolchain
 
+tar -xvf toolchain.tar.gz
